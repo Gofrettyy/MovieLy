25125a4f73c660fdff2607c828e34bdd96213a75
Gofrettyy <naz_1216@outlook.com>
2025-03-04 21:47:51 +0300

    Appwrite Database Added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import {useDebounce} from "react-use";
+import {updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL ="https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -43,6 +44,15 @@ const App = () => {
         return
     }
     setMovieList(data.results || []);
+    if(query && data.results.length > 0 ){
+        await updateSearchCount(query , data.results[0]);
+    }
+    updateSearchCount(
+        {
+            searchTerm: query,
+            count: data.results.length
+        }
+    )
         }
         catch(error)
         {
