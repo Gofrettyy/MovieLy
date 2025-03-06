48bfdfed66505f58b9c9ed884be976543d6f21e0
Gofrettyy <naz_1216@outlook.com>
2025-03-06 23:02:40 +0300

    Trending Movies Section Added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import {useDebounce} from "react-use";
-import {updateSearchCount} from "./appwrite.js";
+import {getTrendingMovies, updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL ="https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -21,6 +21,7 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState(' ');
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
+    const [trendingMovies, setTrendingMovies] = useState([]);
     const [isLoading    , setIsLoading] = useState(false);
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
@@ -63,9 +64,24 @@ const App = () => {
             setIsLoading(false);
         }
     }
+
+    const loadTrendingMovies = async () => {
+        try{
+            const movies = await getTrendingMovies();
+            setTrendingMovies(movies);
+        }
+        catch (error) {
+            console.error(`Error Fetching Trending Movies:${error}`);
+
+        }
+    }
     useEffect(() => {
      fetchMovies(debouncedSearchTerm)
     } , [debouncedSearchTerm]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    } , []) ; // It will only get call at the start
     return (
 
         <main>
@@ -76,8 +92,20 @@ const App = () => {
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
+
+                {trendingMovies.length > 0 && (<section className="trending">
+                    <h2>Trending Now</h2>
+                    <ul>
+                        {trendingMovies.map((movie , index) => (
+                            <li key={movie.$id}>
+                                <p>{index + 1 }</p>
+                                <img src={movie.poster_url} alt={movie.title}/>
+                            </li>
+                        ))}
+                    </ul>
+                </section>)}
             <section className="all-movies">
-            <h2 className = "mt-[40px]">All Movies</h2>
+            <h2>All Movies</h2>
                 {isLoading ?
                     (<Spinner />)
                     : errorMessage ? (<p className="text-red-500">{errorMessage}</p>) :
--- src/appwrite.js
+++ src/appwrite.js
@@ -46,6 +46,7 @@ export const getTrendingMovies = async () => {
         ])
 
         return result.documents;
+
     } catch (error) {
         console.error(error);
     }
